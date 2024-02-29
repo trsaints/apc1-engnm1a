f6b4e2f284e6895384e3cace668e2d48261c2177
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-02-29 00:52:44 +0000

    criar programa para calcular media do iesb

--- aulas/aula01/meu_programa.c
+++ aulas/aula01/meu_programa.c
@@ -4,20 +4,32 @@ int main() {
   float nota1 = 0.0;
   float nota2 = 0.0;
 
-  printf("Entre com a nota1: ");
+  printf("Digite a 1a nota: ");
   scanf("%f", &nota1);
 
-  printf("Entre com a nota2: ");
+  printf("Digite a 2a nota: ");
   scanf("%f", &nota2);
+
+  while (nota1 > 10 | nota1<0 | nota2> 10 | nota2 < 0) {
+    if (nota1 > 10 | nota1 < 0) {
+      printf("Nota inválida. Digite a 1a nota: ");
+      scanf("%f", &nota1);
+    }
+
+    if (nota2 > 10 | nota2 < 0) {
+      printf("Nota inválida. Digite a 2a nota: ");
+      scanf("%f", &nota2);
+    }
+  }
 
   float media = 0.4 * nota1 + 0.6 * nota2;
 
-  printf("A média final é %.1f\n", media);
+  printf("A media do aluno foi: %.1f", media);
 
   if (media >= 5.0) {
-    printf("Passei!\n");
+    printf("\nO aluno foi aprovado!");
   } else {
-    printf("Vamos de P3\n");
+    printf("\nO aluno foi reprovado.");
   }
 
   return 0;
