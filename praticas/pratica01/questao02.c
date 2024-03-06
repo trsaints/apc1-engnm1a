152989b46627619ee30dae206adef7e72b996ad5
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-03-06 22:59:25 +0000

    Criar material correspondente a segunda aula de APC

---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float base = 2;
+    float altura = 2;
+    float area = (base * altura) / 2;
+
+    printf("\nA área do triângulo é: %fm\n", area);
+
+    return 0;
+}
