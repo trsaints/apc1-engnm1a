0e96b5510d52a785c90cf0f679b4c439f98dc325
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-03-07 00:36:36 +0000

    Complementar exibição na atividade 04

--- praticas/pratica01/questao04.c
+++ praticas/pratica01/questao04.c
@@ -12,7 +12,12 @@ int main() {
     float valor_base = 1000.0f;
     float preco_final = calcular_preco_final(valor_base);
 
-    printf("Preço inicial: \t\t%f\nPreço final: \t\t%f\n", valor_base, preco_final);
+    printf("Preço inicial: \t\t%f\n", valor_base);
+    printf("Preço final: \t\t%f\n", preco_final);
+    printf("ICMS: %f\n", (ICMS * valor_base));
+    printf("COFINS: %f\n", (COFINS * valor_base));
+    printf("PIS/PASEP: %f\n", (PIS_PASEP * valor_base));
+    printf("Total de tributos: %f\n", (preco_final - valor_base));
 
     return 0;
 }
