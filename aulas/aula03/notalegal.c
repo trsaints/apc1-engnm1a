d495ffe77611a0a3a7247919192ba834cfe7d11e
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2024-03-13 22:55:51 +0000

    criar o modelo de impressão de NF

---
+++ aulas/aula03/notalegal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("------------------------------\n");
+    printf("           NOTA LEGAL         \n");
+    printf("------------------------------\n");
+    printf("ITEM\t\t\tQTD\tVALOR         \n");
+    printf("%s\t\t%i\t%f\n", "Banana nanica", 5, 15.99);
+    printf("%s\t\t%i\t%f\n", "Maçã fuji", 9, 5.99);
+    printf("%s\t\t%i\t%f\n", "Uva globo", 12, 8.99);
+    printf("------------------------------\n");
+    printf("TOTAL................\t\t26.00\n");
+
+    return 0;
+}
